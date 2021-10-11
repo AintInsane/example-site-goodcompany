ab59c82401073745c292e7d7af220d7ab1aaa702
dogmastring <dogmastring@gmail.com>
2021-10-11 20:49:29 +0300

    Логика сбрасывания позиции скрола при клике по ссылке

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,18 @@
-import { Switch, Route, Redirect } from 'react-router-dom'
+import React from 'react'
+import { Switch, Route, Redirect, useLocation } from 'react-router-dom'
 import About from "../../pages/About/About"
 import Home from "../../pages/Home/Home"
 import Random from "../../pages/Random/Random"
 import "./App.css"
 
 export default function App() {
+
+    const location = useLocation()
+
+    React.useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [location.pathname])
+
     return (
         <div className="App">
             <Switch>
